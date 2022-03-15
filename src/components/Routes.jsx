282d33434783414da062242063a73d7e53cd95a3
Attah David 'Joche <39128641+JocheAttah@users.noreply.github.com>
2022-03-15 16:59:09 +0100

    fix

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import { Dashboard, Listing } from "../pages";
+import {  Listing } from "../pages";
 
 const Routes = () => {
   return (
     <Switch>
       <Route path="/" exact component={Listing} />
-      {/* <Route path="/dashboard" exact component={Dashboard} /> */}
     </Switch>
   );
 };
